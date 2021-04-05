76d374d97fc3b69962d1f41dab71e79fb4004e59
Ollie <3martynov@gmail.com>
2021-04-05 11:21:21 -0400

    followup

--- utils/ApiSource.js
+++ utils/ApiSource.js
@@ -3,13 +3,13 @@ const fetch = require("node-fetch");
 //network helper
 module.exports.ApiSource =   {
     getMoneySupply: async()=>{
-        return fetch("https://ycharts.com/charts/fund_data.json?annotations=&annualizedReturns=false&calcs=&chartType=interactive&chartView=&correlations=&dateSelection=range&displayDateRange=false&displayTicker=false&endDate=&format=real&legendOnChart=false&note=&partner=basic_2000&quoteLegend=false&recessions=false&scaleType=linear&securities=id%3AI%3ACM1MSSM%2Cinclude%3Atrue%2C%2C&securityGroup=&securitylistName=&securitylistSecurityId=&source=false&splitType=single&startDate=&title=&units=false&useCustomColors=false&useEstimates=false&zoom=1&redesign=true&maxPoints=700")
+        return fetch("https://ycharts.com/charts/fund_data.json?annotations=&annualizedReturns=false&calcs=&chartType=interactive&chartView=&correlations=&dateSelection=range&displayDateRange=false&displayTicker=false&endDate=&format=real&legendOnChart=false&note=&partner=basic_2000&quoteLegend=false&recessions=false&scaleType=linear&securities=id%3AI%3ACM1MSSM%2Cinclude%3Atrue%2C%2C&securityGroup=&securitylistName=&securitylistSecurityId=&source=false&splitType=single&startDate=&title=&units=false&useCustomColors=false&useEstimates=false&zoom=3&redesign=true&maxPoints=700")
     },
     parseResponse:(respJson)=>{
         const moneyData = respJson.chart_data[0][0].raw_data;
         const moneyDataFrom = moneyData[moneyData.length-2];
         const moneyDataTo = moneyData[moneyData.length-1];
-        const moneyDataYearAgo = moneyData[moneyData.length-10];
+        const moneyDataYearAgo = moneyData[moneyData.length-12];
         return {moneyDataFrom, moneyDataTo, moneyDataYearAgo};
     }
 }
